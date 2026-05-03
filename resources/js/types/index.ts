0bff7efce34f552e67bb2ea0ea8fa542f4a4ac9f
Aymen-zhr <aymenzouhari066@gmail.com>
2026-05-03 11:45:23 +0100

    remove unused lib and dependencies

--- resources/js/types/index.ts
+++
@@ -1,5 +0,0 @@
-export type { User, Auth } from './auth';
-export type { TwoFactorSetupData, TwoFactorSecretKey } from './auth';
-export type * from './navigation';
-export type * from './ui';
-export type * from './pages';
