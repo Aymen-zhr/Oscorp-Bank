c1d00d12d1463c37cdfcc204274e6e06a656ea82
Aymen Zouhari <aymenzouhari066@gmail.com>
2026-08-12 14:35:06 +0100

    Improve output buffering and fatal error handling in public index

--- public/index.php
+++ public/index.php
@@ -6,7 +6,17 @@ use Illuminate\Http\Request;
 define('LARAVEL_START', microtime(true));
 
 // Buffer output so headers can still be sent even if downstream code writes early.
-ob_start();
+ob_start(null, 0, PHP_OUTPUT_HANDLER_STDFLAGS);
+register_shutdown_function(function () {
+    $error = error_get_last();
+
+    // If a fatal error occurred, clear any output buffer so the framework can still send the error response.
+    if ($error && ob_get_level() > 0) {
+        while (ob_get_level() > 0) {
+            ob_end_clean();
+        }
+    }
+});
 
 // Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
